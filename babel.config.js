8cbe7a04720710af77dd50e4f904a72793f077ba
Pragya Shakya <surajshakyaofficial@gmail.com>
2026-04-25 19:19:27 +0545

    chore: add path aliasing for clean imports

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: ['module:react-native-config'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@screens': './src/screens',
+          '@navigation': './src/navigation',
+          '@store': './src/store',
+          '@hooks': './src/hooks',
+          '@api': './src/api',
+          '@components': './src/components',
+        },
+      },
+    ],
+  ],
 };
